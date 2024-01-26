cba79de5894cfad92effe1802454a6e5ebc4baf0
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 20:20:32 +0800

    description

---
+++ exercise_6.py
@@ -0,0 +1,8 @@
+# Assignment 5:
+# Do the exercise 1-15 in https://pynative.com/python-basic-exercise-for-beginners/
+# Try do challenge yourself by not checking the "solution"
+
+# Exercise 6:
+# Display numbers divisible by 5 from a list
+# Iterate the given list of numbers and print only those numbers which are divisible by 5
+
